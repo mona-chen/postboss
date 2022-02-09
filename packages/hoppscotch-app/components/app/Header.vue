d23d0820dd67b7ca18da6ea15ae22d3f18b2d687
mona <monaoffice20@gmail.com>
2022-02-09 01:24:45 +0000

    patched header

--- packages/hoppscotch-app/components/app/Header.vue
+++ packages/hoppscotch-app/components/app/Header.vue
@@ -9,7 +9,7 @@
           :label="t('app.name')"
           to="/"
         />
-        <AppGitHubStarButton class="mt-1.5 transition <sm:hidden" />
+        <!-- <AppGitHubStarButton class="mt-1.5 transition <sm:hidden" /> -->
       </div>
       <div class="inline-flex items-center space-x-2">
         <ButtonSecondary
